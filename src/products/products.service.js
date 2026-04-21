a4b65e5760b5fb737098b9d57a8b7c0bd4e4a7d0
Vimal Raveendran <vimalraveendra@gmail.com>
2026-04-21 09:53:01 +0200

    feat:implement products service logic

---
+++ src/products/products.service.js
@@ -0,0 +1,51 @@
+export const getCategories = (products) => {
+  const categoryList = products.map((p) => {
+    if (p.category === "men's clothing") return "men";
+    if (p.category === "women's clothing") return "women";
+    return p.category;
+  });
+
+  return ["all", ...new Set(categoryList)];
+};
+
+export const filteredProducts = (products, filters) => {
+  const { searchText, category, maxPrice } = filters;
+  let categoryType = category;
+  if (category === "men") categoryType = "men's clothing";
+  if (category === "women") categoryType = "women's clothing";
+  return products.filter(
+    (p) =>
+      (!searchText ||
+        p.title.toLowerCase().indexOf(searchText.toLowerCase()) > -1) &&
+      (categoryType === "all" || p.category === categoryType) &&
+      (!maxPrice || p.price < maxPrice),
+  );
+};
+
+export const sortedProducts = (products, sortText) => {
+  return products.sort((a, b) => {
+    let key = sortText.split("-")[0];
+    let order = sortText.split("-")[1];
+    if (key === "title") return a[key].localeCompare(b[key]);
+    if (key === "rating" && a[key]["rate"] !== b[key]["rate"])
+      return b[key]["rate"] - a[key]["rate"];
+    if (key !== "rating" && a[key] !== b[key]) {
+      if (order === "desc") {
+        return b[key] - a[key];
+      } else {
+        return a[key] - b[key];
+      }
+    }
+  });
+};
+
+export function getVisibleProducts(products, productStore) {
+  products = filteredProducts(products, {
+    searchText: productStore.filters.searchText,
+    category: productStore.filters.category,
+    maxPrice: productStore.filters.maxPrice,
+  });
+  products = sortedProducts(products, productStore.filters.sortText);
+
+  return products;
+}
